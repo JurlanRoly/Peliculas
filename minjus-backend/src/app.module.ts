33f764fbc21b3d443e6b2e9827825ea33691ac59
Jurlan <jurlan.roly@gmail.com>
2023-07-05 20:47:38 -0400

    Cambio1
    Cambio1

--- minjus-backend/src/app.module.ts
+++ minjus-backend/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PriorityModule } from './priority/priority.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Priority } from './priority/entities/priority.entity';
 import { PeliculaModule } from './pelicula/pelicula.module';
 import { Pelicula } from './pelicula/entities/pelicula.entity';
 
@@ -14,12 +12,11 @@ import { Pelicula } from './pelicula/entities/pelicula.entity';
       host: 'localhost',
       port: 5432,
       username: 'postgres',
-      password: 'spirit',
-      database: 'instacor',
-      entities: [Priority, Pelicula],
+      password: 'asdASD',
+      database: 'bdpelicula',
+      entities: [Pelicula],
       synchronize: false,
     }),
-    PriorityModule,
     PeliculaModule,
   ],
   controllers: [AppController],
